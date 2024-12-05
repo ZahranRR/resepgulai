907116851d6d3ba9c1050f7f0b2afe97659ee7d5
ZahranRR <zahranrrosyadi@gmail.com>
2024-12-05 08:15:03 +0700

    Menambahkan Bahan

---
+++ resepgulai.php
@@ -0,0 +1,14 @@
+<?php
+
+echo "resep gulai ayam";
+
+echo "Bahan:";
+echo "Ayam 1KG";
+echo "Air 500ml";
+echo "Bawang Putih 4";
+echo "Bawang Merah 8";
+echo "Bumbu Instan Gulai 1";
+echo "Santan Kemasan 2";
+echo "Garam, Gula dan Totole 1 Sdt";
+echo "Minyak Goreng";
+echo "Daun Jeruk 3 helai";
